11529f459c43c391225234d3e0db3481efb5ddab
chayeongha <ckdudgk123@naver.com>
2020-01-13 09:37:53 +0900

    Initial commit

---
+++ src/main/java/com/cookie/basic/CookieApplication.java
@@ -0,0 +1,13 @@
+package com.cookie.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieApplication.class, args);
+	}
+
+}
