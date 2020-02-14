ec3260eecfcffadcf1c481c4e82f6eb99f51d463
Sliver8910 <qodmstn1@naver.com>
2020-02-14 12:33:06 +0900

    213

--- src/main/java/com/cookie/basic/config/InterceptorConfig.java
+++ src/main/java/com/cookie/basic/config/InterceptorConfig.java
@@ -35,8 +35,8 @@ public class InterceptorConfig implements WebMvcConfigurer{
 		.addPathPatterns("/member/*")
 		.excludePathPatterns("/member/memberLogin")
 		.excludePathPatterns("/member/selectJoin")
-		.excludePathPatterns("/member/memberJoin")
-		.excludePathPatterns("/member/memberIndex")//빼야함
+		.excludePathPatterns("/member/memberJoin");
+		/*.excludePathPatterns("/member/memberIndex")//빼야함
 		.excludePathPatterns("/member/emailCheck")//빼야함
 		.excludePathPatterns("/member/idCheck")//빼야함.
 		.excludePathPatterns("/member/nickCheck")//빼야함.
@@ -48,6 +48,6 @@ public class InterceptorConfig implements WebMvcConfigurer{
 		.excludePathPatterns("/member/pwSearch")//뺴야함.
 		.excludePathPatterns("/member/sendEmail")//뺴야함.
 		.excludePathPatterns("/member/phoneCheck");//뺴야함.
-		//멤버페이지로 들어왔을떄 패턴등록.
+*/		//멤버페이지로 들어왔을떄 패턴등록.
 	}
 }
